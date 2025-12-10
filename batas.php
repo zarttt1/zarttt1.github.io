d6a4c215e9d9f2c1794a9293f5c2165fe0f75d19
hapis <149855246+zarttt1@users.noreply.github.com>
2025-12-10 14:19:19 +0700

    Add files via upload

---
+++ batas.php
@@ -0,0 +1,1 @@
+<?php header('Content-Type: application/json'); echo file_get_contents('../geojson/batas_wilayah.geojson'); ?>
---
+++ titik.php
@@ -0,0 +1,1 @@
+<?php header('Content-Type: application/json'); echo file_get_contents('../geojson/titik_digitasi.geojson'); ?>
